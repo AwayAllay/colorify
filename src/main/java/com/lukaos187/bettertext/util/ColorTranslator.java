4f2c7ace669661a3fc273fff02199b7e5dd14913
Lukaos187 <lukas.jensen@web.de>
2024-10-28 10:56:03 +0100

    SAVED

--- src/main/java/com/lukaos187/bettertext/util/ColorTranslator.java
+++ src/main/java/com/lukaos187/bettertext/util/ColorTranslator.java
@@ -2,6 +2,9 @@ package com.lukaos187.bettertext.util;
 
 import org.bukkit.ChatColor;
 
+import javax.annotation.Nullable;
+import java.awt.*;
+
 public class ColorTranslator {
 
     /**Translates the colors form the given String with '&' or from hexColors.*/
@@ -10,11 +13,12 @@ public class ColorTranslator {
         String[] text = toColor.split("(?=&)");
         StringBuilder finalMessage = new StringBuilder();
 
+        for (int i = 0; i < text.length; i++) {
 
-        for (String part : text) {
+            String part = text[i];
 
             if (part.startsWith("&#") && part.length() > 7) {
-
+                //&#484848
                 String hexCode = part.substring(1, 8);
 
 
@@ -26,6 +30,12 @@ public class ColorTranslator {
                     finalMessage.append(part);
                 }
             }
+            else if (part.startsWith("&(")) {
+
+
+
+
+            }
             else if (part.startsWith("&") && part.length() >= 2) {
 
                 char colorCode = part.charAt(1);
@@ -33,6 +43,9 @@ public class ColorTranslator {
                 if (ChatColor.getByChar(colorCode) != null) {
                     finalMessage.append(ChatColor.getByChar(colorCode));
                     finalMessage.append(part.substring(2));
+                }
+                else {
+                    finalMessage.append(part);
                 }
 
             }
@@ -42,6 +55,7 @@ public class ColorTranslator {
         }
 
         return finalMessage.toString();
+
     }
 
     /**Tests if the given color is a valid hexColor.*/
@@ -53,6 +67,61 @@ public class ColorTranslator {
         }catch (IllegalArgumentException e){
             return  false;
         }
+    }
+
+    @Nullable
+    private net.md_5.bungee.api.ChatColor getColor(String colorString){
+
+        if (colorString.startsWith("&#") && colorString.length() > 7){
+
+            String hexCode = colorString.substring(1, 8);
+
+            if (isValidHexColor(hexCode)){
+                return net.md_5.bungee.api.ChatColor.of(hexCode);
+            }
+
+        }
+        else if (colorString.startsWith("&") && colorString.length() >= 2) {
+
+            char colorCode = colorString.charAt(1);
+
+            if (ChatColor.getByChar(colorCode) != null) {
+                return net.md_5.bungee.api.ChatColor.getByChar(colorCode);
+            }
+        }
+
+        return null;
+    }
+
+    /**Makes a transition between two colours for a specific text.*/
+    private String transition(String text, Color startColor, Color endColor) {
+
+        StringBuilder gradientText = new StringBuilder();
+        int textLength = text.length();
+
+        // calculate colour transition
+        double rStep = (endColor.getRed() - startColor.getRed()) / (double) textLength;
+        double gStep = (endColor.getGreen() - startColor.getGreen()) / (double) textLength;
+        double bStep = (endColor.getBlue() - startColor.getBlue()) / (double) textLength;
+
+        // text with transition
+        for (int i = 0; i < textLength; i++) {
+            int r = (int) (startColor.getRed() + (rStep * i));
+            int g = (int) (startColor.getGreen() + (gStep * i));
+            int b = (int) (startColor.getBlue() + (bStep * i));
+
+            // Minecraft Hex-Code
+            gradientText.append("§x")
+                    .append("§").append(Integer.toHexString((r >> 4) & 0xF))
+                    .append("§").append(Integer.toHexString(r & 0xF))
+                    .append("§").append(Integer.toHexString((g >> 4) & 0xF))
+                    .append("§").append(Integer.toHexString(g & 0xF))
+                    .append("§").append(Integer.toHexString((b >> 4) & 0xF))
+                    .append("§").append(Integer.toHexString(b & 0xF))
+                    .append(text.charAt(i));
+        }
+
+        return gradientText.toString();
     }
 
 }
